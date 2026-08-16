78e7b4706c133a69b878f49fb1f41be5e447e2bc
Jayinaksha Vyas <145246695+Jayinaksha@users.noreply.github.com>
2026-08-16 23:31:41 +0530

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.py
@@ -0,0 +1,26 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        water = 0
+        pref = collections.deque()
+        sufx = collections.deque()
+        prefix = [0] * len(height)
+        sufix = [0] * len(height)
+        maxpref = 0
+        maxsufx = 0
+        for i in range(len(height)):
+            if i == 0:
+                pref.append(0)
+            elif height[i-1] > pref[-1]:
+                pref.append(max(prefix[i-1],height[i-1]))
+            prefix[i] = pref[-1]
+        for i in range(len(height)-1, -1, -1):
+            if i == len(height)-1:
+                sufx.append(0)
+            elif height[i+1] > sufx[-1]:
+                sufx.append(max(sufix[i-1],height[i+1]))
+            sufix[i] = sufx[-1]  
+
+        for i in range(len(height)):
+            if prefix[i] > height[i] and sufix[i] > height[i]:
+                water += (min(prefix[i], sufix[i]) - height[i])
+        return water
